7175e4a7eab06c0306916c3a9aff5f82778c37a3
Chitakov <chitakov2606nikita@gmail.com>
2022-06-17 18:33:51 +0300

    Added Lab12

---
+++ Lab12_v2/Lab12_v2/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab12_v2
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
